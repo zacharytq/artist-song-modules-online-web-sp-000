4764a591ced30213242774e2d989247e68e98e38
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 01:27:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
-  include Paramable
+  include Paramable, Memorable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
