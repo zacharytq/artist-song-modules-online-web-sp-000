5095c0ab21b50ed7182cd551b3a50917807f85f8
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 01:13:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable
+  extend Findable
 
   attr_accessor :name
   attr_reader :artist
@@ -10,10 +11,6 @@ class Song
 
   def initialize
     @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
   end
 
   def self.all
