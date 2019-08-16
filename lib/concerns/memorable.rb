d929302120f6b399b71a967203a57d5f987dbc79
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 01:25:06 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,13 @@
 module Memorable
-  def count
-    self.all.length
-  end
+  module ClassMethods
+    def count
+      self.all.length
+    end
 
-  def reset_all
-    self.all.clear
-  end
+    def reset_all
+      self.all.clear
+    end
+
+    module InstanceMethods
+      def initialize
 end
